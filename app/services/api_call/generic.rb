1c1205e8d1322a741248a94106b3e0b86b0097b1
samarth <samarth@payatu.io>
2023-07-26 03:31:26 +0530

    updated typheous generic service for hydra usage

--- app/services/api_call/generic.rb
+++ app/services/api_call/generic.rb
@@ -1,11 +1,7 @@
 module ApiCall
   class Generic < Base
-    def build_request
+    def self.build_request
       Typhoeus::Request.new(@url, method: @method, params: @params, body: @body, headers: @headers)
-    end
-
-    def self.call
-      build_request.run
     end
   end
 end
